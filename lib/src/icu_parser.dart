21da8c29c39310fff326f5c7d23c810697936c34
Fredrik Borgström <fredrik@mp1.se>
2023-05-24 23:54:54 +0200

    Dart 3.0 compatible

--- lib/src/icu_parser.dart
+++ lib/src/icu_parser.dart
@@ -61,20 +61,14 @@ class IcuParser {
 
   Parser get optionalMessageText => (icuEscapedText | icuText).star().flatten();
 
-  Parser get placeholderText => (optionalMessageText &
-          openCurly &
-          messageText &
-          closeCurly &
-          optionalMessageText)
-      .plus()
-      .flatten();
+  Parser get placeholderText =>
+      (optionalMessageText & openCurly & messageText & closeCurly & optionalMessageText).plus().flatten();
 
   // Parser<String> get nonIcuMessageText => normalText.plus().flatten();
 
   // Parser<int> get number => digit().plus().flatten().trim().map<int>(int.parse);
 
-  Parser<String> get id =>
-      (letter() & (word() | char('_')).star()).flatten().trim();
+  Parser<String> get id => (letter() & (word() | char('_')).star()).flatten().trim();
 
   Parser<String> get comma => char(',').trim();
 
@@ -91,35 +85,27 @@ class IcuParser {
 
   var interiorText = undefined();
 
-  Parser<String> get preface =>
-      (openCurly & id & comma).map((values) => values[1]);
+  Parser<String> get preface => (openCurly & id & comma).map((values) => values[1]);
 
   Parser get pluralLiteral => string('plural');
 
-  Parser get pluralClause =>
-      (pluralKeyword & openCurly & interiorText & closeCurly).trim().pick(2);
+  Parser get pluralClause => (pluralKeyword & openCurly & interiorText & closeCurly).trim().pick(2);
 
-  Parser get plural =>
-      preface & pluralLiteral & comma & pluralClause.plus() & closeCurly;
+  Parser get plural => preface & pluralLiteral & comma & pluralClause.plus() & closeCurly;
 
   Parser<String> get selectLiteral => string('select');
 
-  Parser get selectClause =>
-      (id & openCurly & interiorText & closeCurly).trim().pick(2);
+  Parser get selectClause => (id & openCurly & interiorText & closeCurly).trim().pick(2);
 
-  Parser get generalSelect =>
-      preface & selectLiteral & comma & selectClause.plus() & closeCurly;
+  Parser get generalSelect => preface & selectLiteral & comma & selectClause.plus() & closeCurly;
 
-  Parser get genderClause =>
-      (genderKeyword & openCurly & interiorText & closeCurly).trim().pick(2);
+  Parser get genderClause => (genderKeyword & openCurly & interiorText & closeCurly).trim().pick(2);
 
-  Parser get gender =>
-      preface & selectLiteral & comma & genderClause.plus() & closeCurly;
+  Parser get gender => preface & selectLiteral & comma & genderClause.plus() & closeCurly;
 
   Parser get pluralOrGenderOrSelect => plural | gender | generalSelect;
 
-  Parser get pluralOrGenderOrSelectContents =>
-      pluralOrGenderOrSelect.map((result) => result[3]);
+  Parser get pluralOrGenderOrSelectContents => pluralOrGenderOrSelect.map((result) => result[3]);
 
   Parser get contents => pluralOrGenderOrSelect | placeholderText | messageText;
 
@@ -129,18 +115,18 @@ class IcuParser {
 
   // TODO: Tokens can be nested deeper and we'll need to get those too using
   // fold or something
-  Result<List<Token>> parse(String message) {
-    final parsed = (placeholderText.token() |
-            justText.token() |
-            pluralOrGenderOrSelectContents)
-        .parse(message)
-        .map(
-      (element) {
-        return element is List ? List<Token>.from(element) : [element as Token];
-      },
-    );
+  List<Token> parse(String message) {
+    final parsed = (placeholderText.token() | justText.token() | pluralOrGenderOrSelectContents).parse(message);
 
-    return parsed;
+    if (parsed.isFailure) {
+      print('Failed to parse: $message');
+      print(parsed.message);
+      throw Exception('parsing failed');
+    } else {
+      final parsedValue = parsed.value;
+      print('Parsed: $parsedValue');
+      return parsedValue is List ? List<Token>.from(parsedValue) : [parsedValue as Token];
+    }
   }
 
   IcuParser() {
